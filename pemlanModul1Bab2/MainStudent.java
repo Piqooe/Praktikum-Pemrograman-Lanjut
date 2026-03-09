4b975ebe65b9a12f77f5f1558385d9be4eb0fb19
Piqooe <muhamfiqo@gmail.com>
2026-03-09 09:58:51 +0700

    (Pertemuan 2) disable contoh

--- pemlanModul1Bab2/MainStudent.java
+++ pemlanModul1Bab2/MainStudent.java
@@ -2,34 +2,35 @@ package tugas_praktikum.pemlanModul1Bab2;
 
 public class MainStudent {
     public static void main(String[] args) {
-        Student anna = new Student();
-        anna.setName("Anna");
-        anna.setAddress("Malang");
-        anna.setAge(20);
-        anna.setMath(100);
-        anna.setScience(89);
-        anna.setEnglish(80);
-        anna.displayMessage();
+        // Student anna = new Student();
+        // anna.setName("Anna");
+        // anna.setAddress("Malang");
+        // anna.setAge(20);
+        // anna.setMath(100);
+        // anna.setScience(89);
+        // anna.setEnglish(80);
+        // anna.displayMessage();
 
-        // menggunakan constructor lain
-        System.out.println("===================");
-        Student chris = new Student("Chris", "Kediri", 21);
-        chris.setMath(70);
-        chris.setScience(60);
-        chris.setEnglish(90);
-        chris.displayMessage();
+        // // menggunakan constructor lain
+        // System.out.println("===================");
+        // Student chris = new Student("Chris", "Kediri", 21);
+        // chris.setMath(70);
+        // chris.setScience(60);
+        // chris.setEnglish(90);
+        // chris.displayMessage();
 
-        // siswa dengan nama anna dirubah informasi alamat dan umurnya melalui
-        // constructor
-        System.out.println("===================");
-        anna = new Student("anna", "Batu", 18);
-        anna.displayMessage();
+        // // siswa dengan nama anna dirubah informasi alamat dan umurnya melalui
+        // // constructor
+        // System.out.println("===================");
+        // anna = new Student("anna", "Batu", 18);
+        // anna.displayMessage();
 
-        // siswa denagan nama chris dirubah informasi alamat dan umurnya melalui method
-        System.out.println("===================");
-        chris.setAddress("Surabaya");
-        chris.setAge(22);
-        chris.displayMessage();
+        // // siswa denagan nama chris dirubah informasi alamat dan umurnya melalui
+        // method
+        // System.out.println("===================");
+        // chris.setAddress("Surabaya");
+        // chris.setAge(22);
+        // chris.displayMessage();
 
         // membuat objek menggunakan constructor dengan parameter nilai mata pelajaran
         System.out.println("===================");
