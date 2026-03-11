3aea6fb60397835d46a61e07fb7900d9fc92e8d9
Piqooe <muhamfiqo@gmail.com>
2026-03-11 19:42:12 +0700

    (Pertemuan 2) Soal no 4

--- pemlanModul1Bab2/MainStudent.java
+++ pemlanModul1Bab2/MainStudent.java
@@ -1,5 +1,7 @@
 package tugas_praktikum.pemlanModul1Bab2;
 
+import java.util.Scanner;
+
 public class MainStudent {
     public static void main(String[] args) {
         // Student anna = new Student();
@@ -32,12 +34,61 @@ public class MainStudent {
         // chris.setAge(22);
         // chris.displayMessage();
 
-        // membuat objek menggunakan constructor dengan parameter nilai mata pelajaran
-        System.out.println("===================");
-        Student budi = new Student(85, 90, 88);
-        budi.setName("Budi");
-        budi.setAddress("Bandung");
-        budi.setAge(20);
-        budi.displayMessage();
+        // // membuat objek menggunakan constructor dengan parameter nilai mata
+        // pelajaran
+        // System.out.println("===================");
+        // Student budi = new Student(85, 90, 88);
+        // budi.setName("Budi");
+        // budi.setAddress("Bandung");
+        // budi.setAge(20);
+        // budi.displayMessage();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah siswa yang ingin di data: ");
+        int jumlahSiswa = scanner.nextInt();
+        scanner.nextLine();
+        Student[] students = new Student[jumlahSiswa];
+
+        for (int i = 0; i < jumlahSiswa; i++) {
+            System.out.println("\nInput Data Siswa ke-" + (i + 1));
+
+            students[i] = new Student();
+
+            System.out.print("Nama: ");
+            String nama = scanner.nextLine();
+            students[i].setName(nama);
+
+            System.out.print("Alamat: ");
+            String alamat = scanner.nextLine();
+            students[i].setAddress(alamat);
+
+            System.out.print("Umur: ");
+            int umur = scanner.nextInt();
+            students[i].setAge(umur);
+
+            System.out.print("Nilai Matematika: ");
+            int math = scanner.nextInt();
+            students[i].setMath(math);
+
+            System.out.print("Nilai Bahasa Inggris: ");
+            int english = scanner.nextInt();
+            students[i].setEnglish(english);
+
+            System.out.print("Nilai Sains: ");
+            int science = scanner.nextInt();
+            students[i].setScience(science);
+
+            scanner.nextLine();
+        }
+
+        System.out.println("\nHASIL DATA SISWA YANG TELAH DIINPUT");
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("\nSiswa ke-" + (i + 1) + ":");
+            students[i].displayMessage();
+        }
+
+        scanner.close();
     }
 }
