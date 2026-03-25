f0bb6eefb6847d323c42aaec5307a081fa07e711
Piqooe <muhamfiqo@gmail.com>
2026-03-26 05:00:40 +0700

    (pemlanModul2Bab5 - Static Method) Up soal

---
+++ pemlanModul2Bab5/StaticMethod/Aritmatika.java
@@ -0,0 +1,28 @@
+package tugas_praktikum.pemlanModul2Bab5.StaticMethod;
+
+public class Aritmatika {
+    public void hitungPenjumlahan(int a, int b) {
+        int nilai = a + b;
+        System.out.println("nilai penjumlahan adalah : " + nilai);
+    }
+
+    public static void hitungPerkalian(int a, int b) {
+        int nilai = a * b;
+        System.out.println("nilai perkalian adalah : " + nilai);
+    }
+
+    public static void hitungPengurangan(int a, int b) {
+        int nilai = a - b;
+        System.out.println("nilai pengurangan adalah : " + nilai);
+    }
+
+    public static void hitungPerkalian(int a, int b) {
+        int nilai = a * b;
+        System.out.println("nilai perkalian adalah : " + nilai);
+    }
+
+    public static void hitungPengurangan(int a, int b) {
+        int nilai = a - b;
+        System.out.println("nilai pengurangan adalah :" + nilai);
+    }
+}
